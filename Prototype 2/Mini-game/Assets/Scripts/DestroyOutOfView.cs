fe3edffe55262fa1be4b1994573c2d01b9072acc
Dark PC <malush1981@gmail.com>
2024-06-13 15:37:13 +0300

    Add side spawn for animals and new 6 types of animals
    Spawn animals on top and on left/right sides with delay in 3s.
    Changed script destroying animals when they are out of borders (add side borders)

--- Prototype 2/Mini-game/Assets/Scripts/DestroyOutOfView.cs
+++ Prototype 2/Mini-game/Assets/Scripts/DestroyOutOfView.cs
@@ -8,15 +8,19 @@ public class DestroyOutOfView : MonoBehaviour
 
     [SerializeField] float maxTopBound = 35f;
     [SerializeField] float maxBottomBound = -10f;
+    [SerializeField] float maxSideBound = 25f;
 
     #endregion
 
     // Update is called once per frame
     void Update()
     {
-        if((transform.position.z > maxTopBound) || (transform.position.z < maxBottomBound))
+        bool isOverVerticalBounds = (transform.position.z > maxTopBound) || (transform.position.z < maxBottomBound);
+        bool isOverSideBounds = (transform.position.x > maxSideBound) || (transform.position.x < -maxSideBound);
+
+        if (isOverVerticalBounds || isOverSideBounds)
         {
-            if(transform.position.z < maxBottomBound)
+            if((transform.position.z < maxBottomBound) || isOverSideBounds)
             {
                 Debug.Log("Game Over!");
             }
--- Prototype 2/Mini-game/Assets/Scripts/SpawnManager.cs
+++ Prototype 2/Mini-game/Assets/Scripts/SpawnManager.cs
@@ -8,33 +8,67 @@ public class SpawnManager : MonoBehaviour
 
     [SerializeField] GameObject[] animalsPrefabs;
 
+    // Top spawn parameters
+
     [SerializeField] float spawnStartZ = 20f;
     [SerializeField] float spawnSpreadX = 15f;
 
-    Vector3 spawnPosition = Vector3.zero;
+    // Side spawn parameters 
+    
+    [SerializeField] float spawnStartX = 21f;
+    [SerializeField] float minPosZ = 3f;
+    [SerializeField] float maxPosZ = 13f;
 
     float spawnStartDelay = 2f;
-    float spawnInterval = 2f;
+    float spawnInterval = 3f;
 
     // Functions names for Invoke
 
-    string spawnRandomAnimal = "SpawnRandomAnimal";
+    string spawnRandomAnimalTop = "SpawnRandomAnimalTop";
+    string spawnRandomAnimalSide = "SpawnRandomAnimalSide";
 
     #endregion
 
 
     private void Start()
     {
-        InvokeRepeating(spawnRandomAnimal, spawnStartDelay, spawnInterval);
+        InvokeRepeating(spawnRandomAnimalTop, spawnStartDelay, spawnInterval);
+        InvokeRepeating(spawnRandomAnimalSide, spawnStartDelay, spawnInterval);
     }
 
-    private void SpawnRandomAnimal()
+    private void SpawnRandomAnimalTop()
     {
-        spawnPosition = new Vector3(Random.Range(-spawnSpreadX, spawnSpreadX), 0, spawnStartZ);
+        Vector3 spawnPosition = new Vector3(Random.Range(-spawnSpreadX, spawnSpreadX), 0, spawnStartZ);
+        SpawnRandomAnimal(spawnPosition, 180f);
+    }
 
+    private void SpawnRandomAnimalSide()
+    {
+        int randomChoice = Random.Range(0, 2);
+
+        float startPositionX = (randomChoice == 0) ? -spawnStartX : spawnStartX;
+        float rotationY = (randomChoice == 0) ? 90f : -90f;
+        Vector3 spawnPosition = new Vector3(startPositionX, 0f, Random.Range(minPosZ, maxPosZ));
+
+        SpawnRandomAnimal(spawnPosition, rotationY);
+    }
+
+    private void SpawnRandomAnimal(Vector3 spawnPosition, float rotationY = 0)
+    {
         int animalIndex = Random.Range(0, animalsPrefabs.Length);
         GameObject spawnAnimal = animalsPrefabs[animalIndex];
 
-        Instantiate(spawnAnimal, spawnPosition, spawnAnimal.transform.rotation);
+        Quaternion animalRotation;
+
+        if ((int)rotationY != 0)
+        {
+            animalRotation = Quaternion.Euler(spawnAnimal.transform.rotation.x, rotationY, spawnAnimal.transform.rotation.z);
+        }
+        else
+        {
+            animalRotation = spawnAnimal.transform.rotation;
+        }
+
+        Instantiate(spawnAnimal, spawnPosition, animalRotation);
     }
 }
